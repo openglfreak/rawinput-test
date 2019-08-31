ea60b14affcbfd772f9cfcbf11999815cf922a6b
Torge Matthies <openglfreak@googlemail.com>
2019-08-31 16:40:33 +0200

    Minor cleanup and refactoring; No semantic changes.

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
-all: a.exe
+all: a.exe a-msg-only.exe
 
 a.exe: main.cxx
-	x86_64-w64-mingw32-g++ -o a.exe main.cxx -mconsole -static $(CXXFLAGS) $(LDFLAGS)
+	x86_64-w64-mingw32-g++ -o $@ $< -mconsole -static $(CXXFLAGS) $(LDFLAGS) -UMESSAGE_ONLY
+
+# Doesn't work under Wine.
+#a-msg-only.exe: main.cxx
+#	x86_64-w64-mingw32-g++ -o $@ $< -mconsole -static $(CXXFLAGS) $(LDFLAGS) -DMESSAGE_ONLY
--- main.cxx
+++ main.cxx
@@ -1,43 +1,52 @@
 #include <stdio.h>
 #include <windows.h>
 
-bool setup_raw_input(HWND hWnd)
+HWND hWnd;
+
+static inline bool setup_raw_input(HWND hWnd)
 {
-	RAWINPUTDEVICE rid[1];
+	RAWINPUTDEVICE rid;
 
-	rid[0].usUsagePage = 0x01;
-	rid[0].usUsage = 0x02;
-	rid[0].dwFlags = RIDEV_NOLEGACY | RIDEV_INPUTSINK;
-	rid[0].hwndTarget = hWnd;
+	rid.usUsagePage = 0x01;
+	rid.usUsage = 0x02;
+	rid.dwFlags = RIDEV_NOLEGACY | RIDEV_INPUTSINK;
+	rid.hwndTarget = hWnd;
 
-	return !!RegisterRawInputDevices(rid, sizeof rid / sizeof rid[0], sizeof rid[0]);
+	return !!RegisterRawInputDevices(&rid, 1, sizeof rid);
 }
 
-bool _handle_raw_input(RAWINPUT* raw)
+static inline char const* rim_type_to_string(DWORD type)
 {
-	const char* type_str;
-	switch (raw->header.dwType)
+	switch (type)
 	{
 		case RIM_TYPEMOUSE:
-			type_str = "mouse";
-			break;
+			return "mouse";
 		case RIM_TYPEKEYBOARD:
-			type_str = "keyboard";
-			break;
+			return "keyboard";
 		case RIM_TYPEHID:
-			type_str = "hid";
-			break;
-		default:
-			return false;
+			return "hid";
 	}
-	printf("header.dwType: %s\n", type_str);
-	printf("header.dwSize: %u\n", raw->header.dwSize);
-	printf("sizeof(RAWINPUT): %llu\n", sizeof(RAWINPUT));
-	if (raw->header.dwType == RIM_TYPEMOUSE)
-	{
-		printf("usFlags: %ui ", (unsigned)raw->data.mouse.usFlags);
-		printf("X: %li Y: %li E: %uli\n", raw->data.mouse.lLastX, raw->data.mouse.lLastY, raw->data.mouse.ulExtraInformation);
-	}
+	return 0;
+}
+
+static inline bool handle_raw_input_2(RAWINPUT const& raw)
+{
+	char const* type_str = rim_type_to_string(raw.header.dwType);
+	if (!type_str)
+		return false;
+
+	printf("header.dwType: %s\n"
+	       "header.dwSize: %u\n",
+	       (char const*)type_str,
+	       (unsigned int)raw.header.dwSize);
+
+	if (raw.header.dwType == RIM_TYPEMOUSE)
+		printf("usFlags: %ui X: %li Y: %li Extra: %uli\n",
+		       (unsigned int)raw.data.mouse.usFlags,
+		       (long int)raw.data.mouse.lLastX,
+		       (long int)raw.data.mouse.lLastY,
+		       (unsigned long int)raw.data.mouse.ulExtraInformation);
+
 	return true;
 }
 
@@ -55,7 +64,7 @@ bool handle_raw_input(HRAWINPUT hRawInput)
 		return false;
 	}
 
-	bool ret = _handle_raw_input((RAWINPUT*)lpb);
+	bool ret = handle_raw_input_2(*(RAWINPUT*)lpb);
 	delete[] lpb;
 	return ret;
 }
@@ -64,9 +73,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
+#ifndef MESSAGE_ONLY
 		case WM_SHOWWINDOW:
 			SetActiveWindow(hWnd);
 			break;
+#endif
 		case WM_INPUT:
 			if (!handle_raw_input((HRAWINPUT)lParam))
 				PostQuitMessage(4);
@@ -78,23 +89,36 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
 
+#if 0 // Does not work under Wine, did not test on Windows.
+BOOL WINAPI ctrl_handler(DWORD fdwCtrlType)
+{
+	CloseWindow(hWnd);
+	DestroyWindow(hWnd);
+	return TRUE;
+}
+#endif
+
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
 	WNDCLASSEX wx = {};
 	wx.cbSize = sizeof(WNDCLASSEX);
 	wx.lpfnWndProc = WndProc;
 	wx.hInstance = hInstance;
-	wx.lpszClassName = "dummy";
+	wx.lpszClassName = "rawinput-test-class";
 	if (!RegisterClassEx(&wx))
 		return 1;
 
 #ifdef MESSAGE_ONLY
-	HWND hWnd = CreateWindowEx(0, wx.lpszClassName, NULL, 0, 0, 0, 0, 0, HWND_MESSAGE, NULL, NULL, NULL);
+	hWnd = CreateWindowEx(0, wx.lpszClassName, NULL, 0, 0, 0, 0, 0, HWND_MESSAGE, NULL, hInstance, NULL);
 #else
-	HWND hWnd = CreateWindowEx(0, wx.lpszClassName, "rawinput-test", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 300, 300, NULL, NULL, hInstance, NULL);
+	hWnd = CreateWindowEx(0, wx.lpszClassName, "rawinput-test", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 300, 300, NULL, NULL, hInstance, NULL);
 #endif
 	if (hWnd == NULL)
 		return 2;
+
+#if 0
+	SetConsoleCtrlHandler(ctrl_handler, TRUE);
+#endif
 
 	if (!setup_raw_input(hWnd))
 		return 3;
